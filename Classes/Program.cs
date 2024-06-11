54092109dc809da5451c133d0c7a50df8a902401
Derrick <Avirajoesph@yahoo.com>
2024-06-11 13:50:06 -0500

    fixed project

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!")
+
+        Car car1 = new Car();
+            car1.year = 1;
+            car1.Make = "aventador";
+            car1.Model = "lamborghini";
         }
     }
+}
+public class Car
+{
+    public Car() 
+    {
+    }
+public string Make { get; set; }
+    public string Model { get; set; }
+    public int year { get; set; }
 }
